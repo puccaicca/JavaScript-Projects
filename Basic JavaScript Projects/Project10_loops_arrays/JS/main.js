0c42d049532553a61a5be1134280f0701dc36ccd
Lilian Tan <lilytan6007@gmail.com>
2022-10-09 21:20:27 -0700

    Add Project 10

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,12 @@
+// Excute a while Loop
+// This counts 1 to 10
+
+function Call_Loop() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
