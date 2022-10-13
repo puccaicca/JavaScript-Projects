71aa622ee27d873005b10d8199fb400a1a7699af
Lilian Tan <lilytan6007@gmail.com>
2022-10-12 21:54:57 -0700

    Add comments to code

--- Basic JavaScript Projects/JavaScript Projects/JavaScript/JavaScript_3.js
+++ Basic JavaScript Projects/JavaScript Projects/JavaScript/JavaScript_3.js
@@ -1,3 +1,7 @@
+// HTML Data Attributes
+// will output a clickable list that states which universe the 
+// character exists in
+
 function displayType(character) {
     var characterType = character.getAttribute("data-character-type");
     alert(characterType + " is in the " + character.innerHTML + " universe!");
