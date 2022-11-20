d75f38aab0442d316f25aceae9cd009ac6553936
Lilian Tan <lilytan6007@gmail.com>
2022-11-19 22:28:18 -0800

    Add remove button functionality

--- ToDo App/js/todo.js
+++ ToDo App/js/todo.js
@@ -51,3 +51,21 @@ function show() {
 document.getElementById('add').addEventListener('click', add);
 // this will keep the inputs displayed permanently on the screen
 show();
+
+// this creates the functionality of removing a todo item from the array
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+    //this looks in the show() how to display a removed item on the screen
+    show();
+
+    return false;
+}
+
+// this tells the browser how to display the todo array after an item has been removed
+var buttons = document.getElementsByClassName('remove');
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', remove);
+};
